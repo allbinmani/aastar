3b422ef07e538f38b7d3456187cf67adb8287d79
origo <mattias@allbinary.se>
2016-10-15 13:42:01 +0200

    Added LICENSE file and license header to all src files

--- src/NodeHeap.h
+++ src/NodeHeap.h
@@ -1,4 +1,12 @@
-
+/*
+ * --------------------------------------------------------------------------
+ * THE "BUY-ME-A-BEER LICENSE" (Revision 0.1):
+ * <mattias@allbinary.se> wrote this code.  As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you 
+ * think my work is worth it (or you think it's worth it anyway), you can buy
+ * me a beer in return. I love beer.
+ * --------------------------------------------------------------------------
+ */
 #ifndef __NODEHEAP_H__
 #define __NODEHEAP_H__ 1
 
